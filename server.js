e9f0b7a7b3bfbd06550067fe99d118988a2c9eb1
Basit Ali Jahan <basitalijahan@gmail.com>
2026-06-01 02:40:59 +0500

    Fixed server

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
+// 404 handler - FIXED: Use '/*' instead of '*' for Express 5 compatibility
+app.use('/*', (req, res) => {
   res.status(404).json({ error: 'Route not found', path: req.originalUrl });
 });
 
